6e2c89b043fa2d955891fdda69f8e1c5940b8d75
ff-4343 <ff-4343@hotmail.com>
2025-08-10 13:07:50 +0400

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,10 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
-// Graceful shutdown
 process.on('SIGINT', async () => {
   try { await prisma.$disconnect(); } finally { process.exit(0); }
 });
-process.on('SIGTERM', async () => {
-  await prisma.$disconnect();
-});
+process.on('SIGTERM', async () => { await prisma.$disconnect(); });
